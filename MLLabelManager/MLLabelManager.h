23f2fb0ea266ee2a70d68ba8b8c42ab081d4b11a
levy <lqcqc@sina.com>
2016-12-06 20:14:42 +0800

    Add Carthage
    Add Carthage

---
+++ MLLabelManager/MLLabelManager.h
@@ -0,0 +1,22 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for MLLabelManager.
+FOUNDATION_EXPORT double MLLabelManagerVersionNumber;
+
+//! Project version string for MLLabelManager.
+FOUNDATION_EXPORT const unsigned char MLLabelManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLLabelManager/PublicHeader.h>
+
+
+#import <MLLabelManager/MLLabel.h>
+#import <MLLabelManager/MLLinkLabel.h>
+#import <MLLabelManager/MLLabelLayoutManager.h>
+#import <MLLabelManager/NSString+MLExpression.h>
+#import <MLLabelManager/NSAttributedString+MLExpression.h>
+#import <MLLabelManager/MLTextAttachment.h>
+#import <MLLabelManager/MLExpressionManager.h>
+#import <MLLabelManager/NSString+MLLabel.h>
+#import <MLLabelManager/NSMutableAttributedString+MLLabel.h>
+#import <MLLabelManager/NSAttributedString+MLLabel.h>
+#import <MLLabelManager/MLLabel+Override.h>
